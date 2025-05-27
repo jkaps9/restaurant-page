6e46cd68495286cce2b4981fd9e660df649d8585
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-27 00:12:20 -0400

    Fix dev config file

--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
     ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
     module: {
         rules: [
             {
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,12 +3,6 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
